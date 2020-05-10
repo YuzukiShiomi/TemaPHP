274734ebb0069e48acd51110827e75c49e0f9128
Rocío Franco <willawe@gmail.com>
2020-05-10 19:59:29 +0200

    adding more functionality to product class

--- src/Entity/Producto.php
+++ src/Entity/Producto.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="productos")
  * @ORM\Entity
  */
-class Producto
+class Producto implements \JsonSerializable
 {
     /**
      * @ORM\Column(name="id", type="integer", nullable=false)
@@ -37,5 +37,92 @@ class Producto
      */
     protected float $price;
 
+    /**
+     * Producto constructor.
+     * @param string $name
+     * @param float $price
+     * @param string $description
+     */
+    public function __construct(string $name = '', float $price = 0, ?string $description = null)
+    {
+        $this->name = $name;
+        $this->price = $price;
+        $this->description = $description;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): Producto
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string|null $description
+     */
+    public function setDescription(?string $description): Producto
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * @param $price
+     * @return Producto
+     */
+    public function setPrice($price): Producto
+    {
+        $this->price = $price;
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return sprintf('Product [id=' . $this->getId() . ', name=' . $this->getName() . ']');
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'description' => $this->getDescription(),
+            'price' => $this->getPrice()
+        ];
+    }
 
 }
